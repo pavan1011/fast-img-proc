7d352e0a92f4a6081119a03d825a3f6405e538f9
pavan1011 <pavan1011@gmail.com>
2025-01-21 08:32:56 -0500

    Updated script to handle input file paths

--- include/cpu/gauss_blur.h
+++ include/cpu/gauss_blur.h
@@ -7,9 +7,17 @@
 #include "image/image.h"
 
 namespace cpu {
-    // Gaussian blur with configurable kernel size
-    // kernel_size must be odd and >= 3
-    // Default values correspond to 3x3 implementation
+    /**
+     * Performs Gaussian blur with configurable kernel on the CPU.
+     *
+     * @param input The input image of type Image::Image (must be 1 or 3 channels).
+     * @param kernel_size: An integer that specifies a square kernel (must be odd and >= 3 and <= 29)
+     *                     Default size is 3x3
+     * @param sigma: The standard deviation value used 1.0 by default. 
+     *               Negative values results in automatic calculation based on kernel size.
+     * @return Resultant image after applying gaussian blur.
+    *  @throws std::invalid_argument if invalid kernel size is passed (supported = 1, 3, 5, and 7)
+     */
     Image gaussian_blur(const Image& input, 
                        int kernel_size = 3, 
                        float sigma = 1.0f);
