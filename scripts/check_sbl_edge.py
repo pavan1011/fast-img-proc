7b84daae3aaabb9219fc042537452040b8b20686
pavan1011 <pavan1011@gmail.com>
2025-01-22 14:06:40 -0500

    Improved to accept input and output dir

--- scripts/check_sbl_edge.py
+++ scripts/check_sbl_edge.py
@@ -1,162 +1,96 @@
 import fast_image_processing as fip
 import time
+import os
+from os import listdir
+from os.path import isfile, join, isdir
+import sys
+
+def print_help():
+    # If output_dir is not passed as cmd line arg, a new dir called "output_check_sbl_edge is created."
+    print("Correct usage: python3 check_sbl_edge.py <input_dir or input_image> <optional: output_dir>")
+
+def run_sbl_edge(img_file, output_dir):
+    
+    test_ksizes = [3, 5, 7]
+    test_dx_dy_list = [[0, 1], [1, 0], [1, 1]]
+    test_hardware = {"CPU" : fip.Hardware.CPU, 
+                     "GPU" : fip.Hardware.GPU}
+    
+    input_filename, input_file_ext = os.path.splitext(os.path.basename(img_file))
+    input_image = fip.Image(img_file)
+
+    for hw in test_hardware.keys():
+        print(f"============  {hw}  ============")
+        for ksize in test_ksizes:
+            for dx_dy in test_dx_dy_list:
+                print(f"--- {input_filename}{input_file_ext} ---")
+                output_filename = "output_" + \
+                                    f"{input_filename}_" + \
+                                    f"{dx_dy[0]}_" + \
+                                    f"{dx_dy[1]}_" + \
+                                    f"{ksize}_" + \
+                                    f"{hw}{input_file_ext}"
+                
+                start_time = time.time()
+                cpu_result = fip.edge_detect(input_image, 
+                                                dx_dy[0], 
+                                                dx_dy[1], 
+                                                ksize, 
+                                                test_hardware[hw])
+                end_time = time.time()
+
+                if (cpu_result.save(f"{output_dir}/{output_filename}")):
+                    print(f"Saved {output_filename}")
+                else:
+                    print(f"Error saving {output_filename}")
+                print("--- %s seconds ---" % (time.time() - start_time))
 
 def main():
 
-    start_time = time.time()
-    # Load an RGB image (PNG or JPG supported)
-    input_image = fip.Image("input.png")
-    print("--- %s seconds ---" % (time.time() - start_time))
+    len_cmd_args = len(sys.argv)
 
-    start_time = time.time()
-    # Derivatives on x-axis, smoothing on y-axis, kernel_size = 5
-    cpu_edge_1_0_3 = fip.edge_detect(input_image, 1, 0, 3, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))    
-    cpu_edge_1_0_3.save("edge_1_0_3_cpu.png")
+    if len_cmd_args > 1 and len_cmd_args <= 3:
+        output_dir = "output_check_sbl_edge"
+        if len_cmd_args == 3:
+            output_dir = os.path.abspath(sys.argv[2])
+        else: 
+            print("No output dir provided." + \
+                  "Creating one called output_check_sbl_edge in current dir")
+        
+        if not os.path.exists(output_dir):
+            print(f"Output directory: {output_dir} doesn't exist. Creating it.")
+            os.makedirs(output_dir)
 
-    start_time = time.time()
-    # Smoothing on x-axis, Derivative on y-axis, kernel_size = 5
-    cpu_edge_0_1_3 = fip.edge_detect(input_image, 0, 1, 3, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_0_1_3.save("edge_0_1_3_cpu.png")
-
-    start_time = time.time()
-    # Derivative on x-axis, Derivative on y-axis, kernel_size = 5
-    cpu_edge_1_1_3 = fip.edge_detect(input_image, 1, 1, 3, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_1_1_3.save("edge_1_1_3_cpu.png")
-
-    start_time = time.time()
-    # Derivatives on x-axis, smoothing on y-axis, kernel_size = 5
-    cpu_edge_1_0_5 = fip.edge_detect(input_image, 1, 0, 5, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_1_0_5.save("edge_1_0_5_cpu.png")
-
-    start_time = time.time()
-    # Derivatives on y-axis, smoothing on x-axis, kernel_size = 5
-    cpu_edge_0_1_5 = fip.edge_detect(input_image, 0, 1, 5, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_0_1_5.save("edge_0_1_5_cpu.png")
-
-    start_time = time.time()
-    # Derivatives on y-axis, smoothing on x-axis, kernel_size = 5
-    cpu_edge_1_1_5 = fip.edge_detect(input_image, 1, 1, 5, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_1_1_5.save("edge_1_1_5_cpu.png")
-
-    # Chess BOARD
-    # Load an RGB image (PNG or JPG supported)
-    input_image = fip.Image("input_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on x-axis, smoothing on y-axis, kernel_size = 5
-    cpu_edge_1_0_3 = fip.edge_detect(input_image, 1, 0, 3, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_1_0_3.save("edge_1_0_3_cpu_tiger.jpg")
-
-    start_time = time.time()
-    # Smoothing on x-axis, Derivative on y-axis, kernel_size = 5
-    cpu_edge_0_1_3 = fip.edge_detect(input_image, 0, 1, 3, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_0_1_3.save("edge_0_1_3_cpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivative on x-axis, Derivative on y-axis, kernel_size = 5
-    cpu_edge_1_1_3 = fip.edge_detect(input_image, 1, 1, 3, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_1_1_3.save("edge_1_1_3_cpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on x-axis, smoothing on y-axis, kernel_size = 5
-    cpu_edge_1_0_5 = fip.edge_detect(input_image, 1, 0, 5, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_1_0_5.save("edge_1_0_5_cpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on y-axis, smoothing on x-axis, kernel_size = 5
-    cpu_edge_0_1_5 = fip.edge_detect(input_image, 0, 1, 5, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_0_1_5.save("edge_0_1_5_cpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on y-axis, smoothing on x-axis, kernel_size = 5
-    cpu_edge_1_1_5 = fip.edge_detect(input_image, 1, 1, 5, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_1_1_5.save("edge_1_1_5_cpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on x-axis, smoothing on y-axis, kernel_size = 5
-    cpu_edge_1_0_7 = fip.edge_detect(input_image, 1, 0, 7, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_1_0_7.save("edge_1_0_7_cpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on y-axis, smoothing on x-axis, kernel_size = 5
-    cpu_edge_0_1_7 = fip.edge_detect(input_image, 0, 1, 7, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_0_1_7.save("edge_0_1_7_cpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on y-axis, smoothing on x-axis, kernel_size = 5
-    cpu_edge_1_1_7 = fip.edge_detect(input_image, 1, 1, 7, fip.Hardware.CPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    cpu_edge_1_1_7.save("edge_1_1_7_cpu_tiger.jpg")
-
-    print("===== GPU ========")
-
-    start_time = time.time()
-    # Derivatives on x-axis, smoothing on y-axis, kernel_size = 5
-    gpu_edge_1_0_3 = fip.edge_detect(input_image, 1, 0, 3, fip.Hardware.GPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    gpu_edge_1_0_3.save("edge_1_0_3_gpu_tiger.jpg")
-
-    start_time = time.time()
-    # Smoothing on x-axis, Derivative on y-axis, kernel_size = 5
-    gpu_edge_0_1_3 = fip.edge_detect(input_image, 0, 1, 3, fip.Hardware.GPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    gpu_edge_0_1_3.save("edge_0_1_3_gpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivative on x-axis, Derivative on y-axis, kernel_size = 5
-    gpu_edge_1_1_3 = fip.edge_detect(input_image, 1, 1, 3, fip.Hardware.GPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    gpu_edge_1_1_3.save("edge_1_1_3_gpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on x-axis, smoothing on y-axis, kernel_size = 5
-    gpu_edge_1_0_5 = fip.edge_detect(input_image, 1, 0, 5, fip.Hardware.GPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    gpu_edge_1_0_5.save("edge_1_0_5_gpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on y-axis, smoothing on x-axis, kernel_size = 5
-    gpu_edge_0_1_5 = fip.edge_detect(input_image, 0, 1, 5, fip.Hardware.GPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    gpu_edge_0_1_5.save("edge_0_1_5_gpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on y-axis, smoothing on x-axis, kernel_size = 5
-    gpu_edge_1_1_5 = fip.edge_detect(input_image, 1, 1, 5, fip.Hardware.GPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    gpu_edge_1_1_5.save("edge_1_1_5_gpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on x-axis, smoothing on y-axis, kernel_size = 5
-    gpu_edge_1_0_7 = fip.edge_detect(input_image, 1, 0, 7, fip.Hardware.GPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    gpu_edge_1_0_7.save("edge_1_0_7_gpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on y-axis, smoothing on x-axis, kernel_size = 5
-    gpu_edge_0_1_7 = fip.edge_detect(input_image, 0, 1, 7, fip.Hardware.GPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    gpu_edge_0_1_7.save("edge_0_1_7_gpu_tiger.jpg")
-
-    start_time = time.time()
-    # Derivatives on y-axis, smoothing on x-axis, kernel_size = 5
-    gpu_edge_1_1_7 = fip.edge_detect(input_image, 1, 1, 7, fip.Hardware.GPU)
-    print("--- %s seconds ---" % (time.time() - start_time))
-    gpu_edge_1_1_7.save("edge_1_1_7_gpu_tiger.jpg")
+        if isdir(os.path.abspath(sys.argv[1])):
+            input_dir = os.path.abspath(sys.argv[1])
+            image_list = []
+            
+            for f in listdir(input_dir):
+                if(isfile(join(input_dir, f)) and \
+                    (f.endswith('.png') or f.endswith('.jpg'))):
+                        print(f"Found: {f}")
+                        image_list.append(join(input_dir, f))
+            
+            print("======================================================")
+            print(f"Calling check_sbl_edge with: ")
+            print(f"input_dir: {input_dir}")
+            print(f"output_dir: {output_dir}")
+            print("======================================================")
+            
+            for img_file in image_list:
+                run_sbl_edge(img_file, output_dir)
+        
+        else:
+            input_file = os.path.abspath(sys.argv[1])
+            if(isfile(input_file) and \
+                    (input_file.endswith('.png') or input_file.endswith('.jpg'))):
+                run_sbl_edge(input_file, output_dir)
+            
+            else:
+                print_help()
+    
+    else:
+        print_help()
 
 if __name__ == "__main__":
     main()
